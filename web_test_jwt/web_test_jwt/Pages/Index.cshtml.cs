89b90b061f33ccffb1d124101dd38b8284a98e7e
Theerapat64003 <theerapatnugud@gmail.com>
2026-02-23 15:13:17 +0700

    refactor code Cookies

--- web_test_jwt/web_test_jwt/Pages/Index.cshtml.cs
+++ web_test_jwt/web_test_jwt/Pages/Index.cshtml.cs
@@ -40,42 +40,18 @@ namespace web_test_jwt.Pages
             if (Username == "admin" && Password == "1234")
             {
                 //HttpContext.Session.SetString("User", Username);
-                
+
                 //return RedirectToPage("Index");
 
 
 
-
-
-
-                var claims = new[]
-                {
-                    new Claim("UserId", "test"),
-                    new Claim("Username", Username),
-                    new Claim("Role", "Admin")
-                };
-
-                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MySuperSecretKey_For_JWT_2026_SecretKey"));
-                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                var userId = "test";
+                var username = Username;
+                var role = "Admin";
+                var secretKey = "MySuperSecretKey_For_JWT_2026_SecretKey";
+                var cookiesKey = "auth_token";
                 var expires = DateTime.UtcNow.AddMinutes(2);
-                var token = new JwtSecurityToken(
-                    issuer: "LoginWeb",
-                    audience: "DashboardWeb",
-                    claims: claims,
-                    expires: expires,
-                    signingCredentials: creds
-                );
-
-                string jwtToken = new JwtSecurityTokenHandler().WriteToken(token); // token ที่คุณสร้างได้
-
-                Response.Cookies.Append("auth_token", jwtToken, new CookieOptions
-                {
-                    HttpOnly = true,              // JS อ่านไม่ได้ (ปลอดภัย)
-                    Secure = true,                // ส่งเฉพาะ HTTPS
-                    SameSite = SameSiteMode.None, // ถ้าเรียกข้าม domain
-                    Path = "/",                   // ใช้ได้ทั้งเว็บ
-                    Expires = expires
-                });
+                SetCookies(userId, username, role, secretKey, cookiesKey, expires);
 
                 LoginStatus = true;
                 Message = "Login สำเร็จ";
@@ -85,15 +61,54 @@ namespace web_test_jwt.Pages
             Message = "Username หรือ Password ไม่ถูกต้อง";
             return Page();
         }
+
+        private void SetCookies(string userId, string username, string role, string secretKey, string cookiesKey, DateTime expires)
+        {
+            var claims = new[]
+                            {
+                    new Claim(ClaimTypes.NameIdentifier , userId),
+                    new Claim(ClaimTypes.Name, username),
+                    new Claim(ClaimTypes.Role, role)
+                };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "LoginWeb",
+                audience: "DashboardWeb",
+                claims: claims,
+                expires: expires,
+                signingCredentials: creds
+            );
+
+            string jwtToken = new JwtSecurityTokenHandler().WriteToken(token); // token ที่คุณสร้างได้
+
+            Response.Cookies.Append(cookiesKey, jwtToken, new CookieOptions
+            {
+                HttpOnly = true,              // JS อ่านไม่ได้ (ปลอดภัย)
+                Secure = true,                // ส่งเฉพาะ HTTPS
+                SameSite = SameSiteMode.None, // ถ้าเรียกข้าม domain
+                Path = "/",                   // ใช้ได้ทั้งเว็บ
+                Expires = expires
+            });
+        }
+
         public IActionResult OnPostLogout()
         {
-            Response.Cookies.Delete("auth_token", new CookieOptions
-            {
-                Path = "/"
-            });
+            var cookiesKey = "auth_token";
+            RemoveCookies(cookiesKey);
             LoginStatus = true;
             Message = "Logout สำเร็จ";
             return Page();
+        }
+
+        private void RemoveCookies(string cookiesKey)
+        {
+            Response.Cookies.Delete(cookiesKey, new CookieOptions
+            {
+                Path = "/"
+            });
         }
     }
 }
